c69c706575bf4c17b5d3bdda456b0a828ddebee9
Martin Siklosi <martin@siklosi.se>
2023-08-05 18:21:30 +0200

    added screen matching notebook

--- lights.py
+++ lights.py
@@ -2,11 +2,13 @@ import os
 import json
 from collections.abc import Iterable
 
+
 import colour
 import requests
 from dotenv import load_dotenv
-load_dotenv()
 
+
+load_dotenv()
 BRIDGE_IP = os.getenv("BRIDGE_IP")
 USERNAME = os.getenv("USERNAME")
 BASE_URL = url = f"http://{BRIDGE_IP}/api/{USERNAME}"
@@ -54,6 +56,21 @@ class Light:
         res = requests.put(url, data=json.dumps(kwargs))
         res.raise_for_status()
         return res.json()
+
+
+def get_bridge_state() -> dict:
+    res = requests.get(BASE_URL)
+    res.raise_for_status()
+    return res.json()
+
+
+def get_lights() -> list[Light]:
+    bridge_state = get_bridge_state()
+    light_items = bridge_state["lights"].items()
+    lights = [Light(i)
+              for i, light in light_items
+              if light["state"]["reachable"]]
+    return lights
 
 
 def rgb_to_xy(rgb: Iterable) -> tuple[int, int]:
---
+++ screen.py
@@ -0,0 +1,25 @@
+from mss import mss
+import numpy as np
+
+
+# def avg_screen_rgb() -> tuple[int, int, int]:
+#     with mss() as sct:
+#         screen = np.array(sct.grab(sct.monitors[0]))
+#         # Dropping the alpha channel if present
+#         screen = screen[..., 2::-1]
+#         average_color = screen.mean(axis=(0, 1))
+#         return tuple(map(int, average_color))
+
+
+def avg_screen_rgb(segments: int = 1) -> list[tuple[int, int, int]]:
+    with mss() as sct:
+        screen = np.array(sct.grab(sct.monitors[0]))
+        # Dropping the alpha channel if present
+        screen = screen[..., 2::-1]
+        segment_width = screen.shape[1] // segments
+        segment_rgb_list = []
+        for i in range(segments):
+            segment = screen[:, i * segment_width: (i + 1) * segment_width, :]
+            average_color = segment.mean(axis=(0, 1))
+            segment_rgb_list.append(tuple(map(int, average_color)))
+        return segment_rgb_list
